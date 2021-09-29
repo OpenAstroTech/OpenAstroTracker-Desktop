55d135af102416ddf063a13a966760c6bcda3164
Chris <christian.kardach@gmail.com>
2021-09-29 08:06:44 +0200

    Added description and copyright info

--- OATSimulation/Properties/AssemblyInfo.cs
+++ OATSimulation/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OATSimulation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Virtual representation of OpenAstroTracker")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OATSimulation")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 by OpenAstroTech")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
